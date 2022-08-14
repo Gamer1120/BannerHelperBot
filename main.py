85e679f64d4f75afc47ffb91313e94b681e10049
Gamer1120 <michaelkoopmantest@gmail.com>
2022-08-14 18:49:34 +0200

    Fix bugs

--- main.py
+++ main.py
@@ -25,34 +25,49 @@ def get_mission_url(mission_id, mission_number):
 
 async def first_mission(update: Update, context: ContextTypes.DEFAULT_TYPE):
     shared_url = update.message.text
-    if 'https://link.ingress.com/?link=' not in shared_url:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text='That is not a correct link. Please '
-                                                                              'share the mission link from the '
-                                                                              'mission overview.')
-    else:
+    mission_details = ''
+    if 'https://bannergress.com/banner/' in shared_url:
+        banner_id = shared_url.replace('https://bannergress.com/banner/', '')
+        resp = requests.get('https://api.bannergress.com/bnrs/' + banner_id)
+        if resp.status_code is not 200:
+            await context.bot.send_message(chat_id=update.effective_chat.id,
+                                           text='Bannergress did not like that mission. Please report the mission to @M1chaeI so he can investigate.')
+            return
+        else:
+            mission_details = resp.json()
+            banner_id = mission_details['missions']['0']['id']
+    elif 'https://link.ingress.com/?link=' in shared_url:
         banner_id = str(shared_url[-35:])
         resp = requests.get('https://api.bannergress.com/bnrs?missionId=' + banner_id)
         if resp.status_code is not 200:
-            await context.bot.send_message(chat_id=update.effective_chat.id, text='Bannergress did not like that mission. Please report the mission to @M1chaeI so he can investigate.')
+            await context.bot.send_message(chat_id=update.effective_chat.id,
+                                           text='Bannergress did not like that mission. Please report the mission to @M1chaeI so he can investigate.')
+            return
         else:
             j = resp.json()
             mission_details = j[0]
-            reply_keyboard = [
-                [InlineKeyboardButton('Open in Scanner', url=get_mission_url(mission_details['id'], "1"))],
-                [InlineKeyboardButton('Next mission', callback_data=str(banner_id) + ':::1')],
-            ]
-            await update.message.reply_text(
-                str(mission_details['title']) + "\n\n"
-                "You are currently on mission: 1 of " + str(mission_details['numberOfMissions']) + "\n\n",
-                reply_markup=InlineKeyboardMarkup(reply_keyboard),
-                parse_mode=ParseMode.MARKDOWN_V2
-            )
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='That is not a correct link. Please '
+                                                                              'share the mission link from the '
+                                                                              'mission overview or a Bannergress link.')
+        return
+    reply_keyboard = [
+        [InlineKeyboardButton('Open in Scanner', url=get_mission_url(mission_details['id'], "1"))],
+        [InlineKeyboardButton('Next mission', callback_data=str(banner_id) + ':::1')],
+    ]
+    await update.message.reply_text(
+        str(mission_details['title']) + "\n\n"
+        "You are currently on mission: 1 of " + str(mission_details['numberOfMissions']) + "\n\n",
+        reply_markup=InlineKeyboardMarkup(reply_keyboard),
+        parse_mode=ParseMode.MARKDOWN_V2
+    )
 
 
 async def next_mission(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
     split = query.data.split(':::')
+    print(split)
     banner_id = split[0]
     mission_number = split[1]
     mission_number = str(int(mission_number) + 1)
@@ -64,23 +79,24 @@ async def next_mission(update: Update, context: ContextTypes.DEFAULT_TYPE):
         j = resp.json()
         print(j[0])
         mission_details = j[0]
-        reply_keyboard = [
-            [InlineKeyboardButton('Open in Scanner', url=get_mission_url(mission_details['id'], str(int(mission_number))))],
-            [InlineKeyboardButton('Previous mission', callback_data=str(banner_id) + ':::' + str(int(mission_number)-2))],
-            [InlineKeyboardButton('Next mission', callback_data=str(banner_id) + ':::' + mission_number)],
-        ]
-        mission_details = j[0]
         total_number_of_missions_in_banner = mission_details['numberOfMissions']
         if int(mission_number) > int(total_number_of_missions_in_banner):
-            await query.edit_message_text(text='Looks like you completed the banner. Congrats!')
+            await query.edit_message_text(text='Looks like you completed the banner. Congrats! Don\'t forget to mark it Done on Bannergress!\n\n'
+                                               'https://bannergress.com/banner/' + mission_details['id'])
         else:
+            reply_keyboard = [
+                [InlineKeyboardButton('Open in Scanner', url=get_mission_url(mission_details['id'], str(int(mission_number))))],
+                [InlineKeyboardButton('Previous mission', callback_data=str(banner_id) + ':::' + str(int(mission_number)-2))],
+                [InlineKeyboardButton('Next mission', callback_data=str(banner_id) + ':::' + mission_number)],
+            ]
+
             await query.edit_message_text(
-                str(mission_details['title']) + "\n\n"
-                                                "You are currently on mission: "+mission_number+" of " + str(
-                    mission_details['numberOfMissions']) + "\n\n",
-                reply_markup=InlineKeyboardMarkup(reply_keyboard),
-                parse_mode=ParseMode.MARKDOWN_V2
-        )
+                    str(mission_details['title']) + "\n\n"
+                                                    "You are currently on mission: "+mission_number+" of " + str(
+                        mission_details['numberOfMissions']) + "\n\n",
+                    reply_markup=InlineKeyboardMarkup(reply_keyboard),
+                    parse_mode=ParseMode.MARKDOWN_V2
+            )
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_ACCESS_TOKEN).build()
